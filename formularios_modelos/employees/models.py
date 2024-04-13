3756626d78c123a8a96b7ee339198838fcf9f810
John Becerra <jfbeccera@gmail.com>
2024-04-13 10:10:41 -0500

    imagenes genrico

--- formularios_modelos/employees/models.py
+++ formularios_modelos/employees/models.py
@@ -5,6 +5,8 @@ class Employee(models.Model):
   first_name = models.CharField(max_length=50, blank=False, null=False)
   last_name  = models.CharField(max_length=50, blank=False, null=False)
   email      = models.EmailField(max_length=50, blank=False, null=False)
+  
+  imagen     = models.ImageField(null=True, blank=True)
 
   def __str__(self):
       return self.first_name
--- formularios_modelos/employees/views.py
+++ formularios_modelos/employees/views.py
@@ -1,18 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import EmployeeForm
+from .models import Employee
 
 # Create your views here.
 def index(request):
-  # return HttpResponse('Correcto')
+  data = {
+    'form': EmployeeForm()
+  }
   if request.method == 'POST':
-    form = EmployeeForm(request.POST)
-    if form.is_valid():
-      # Guardamos la informacion
-      form.save()
-      return render(request, 'index.html', {'form': form })
+    formulario = EmployeeForm(request.POST, files=request.FILES)
+    if formulario.is_valid():
+      # Guardamos la información
+      formulario.save()
+      data['mensaje'] = "Datos guardados correctamente"
+      data['form'] = formulario
+      return render(request, 'index.html', data)
   else:
-    form = EmployeeForm()
-    return render(request, 'index.html', {
-      'form': form
-    })
+    return render(request, 'index.html', data)
+
+def modificar(request, id):
+  empleado = Employee.objects.get(id=id)
+  data = {
+    'form': EmployeeForm(instance=empleado)
+  }
+  if request.method == "POST":
+    formulario = EmployeeForm(data=request.POST, instance=empleado, files=request.FILES)
+    if formulario.is_valid():
+      formulario.save()
+      data['mensaje'] = "Empleado actualizado"
+  return render(request, 'index.html', data)
